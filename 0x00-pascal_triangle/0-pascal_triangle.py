f7794b3f6587ae9ab951336872b177ceb43021a9
ayubsh <ibrahimayubsheikh@gmail.com>
2023-09-29 22:22:38 -0700

    fixed error

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""
-0-pascal_triangle - print
+"""0-pascal_triangle - creates pascal triangle
 """
 
 
 def pascal_triangle(n):
+    """
+        pascal_triangle - creates and returns a list of
+        list rep pascal triangle
+    """
     if n <= 0:
         return []
 
     pascal = [[1]]
 
     for i in range(1, n):
-        """
-            creates the pascal triangle
-        """
         row = [1]
         ls_row = pascal[-1]
         row += [ls_row[j] + ls_row[j+1] for j in range(len(ls_row) - 1)]
