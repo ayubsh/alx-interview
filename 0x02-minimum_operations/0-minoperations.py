d8fee3960a8693159edb4d67a6d9ebd88257a825
ayubsh <ibrahimayubsheikh@gmail.com>
2023-10-15 09:20:58 -0700

    minimun ops

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Minimum opreation """
+
+
+def minOperations(n):
+    """minimun ops
+    """
+    ops_count = 0
+    clipboard = 0
+    done = 1
+
+    while done < n:
+        if clipboard == 0:
+            # Initialize - copy all and first paste
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        elif n - done > 0 and (n - done) % done == 0:
+            # Copy all and paste to double characters
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+
+        else:
+            # Paste to incrementally add characters
+            done += clipboard
+            ops_count += 1
+    return ops_count
