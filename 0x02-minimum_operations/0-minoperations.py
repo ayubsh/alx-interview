58088ea62e5197c976191a0f307b593d2b3a533b
ayubsh <ibrahimayubsheikh@gmail.com>
2023-10-15 10:31:04 -0700

    min ops

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,31 +1,28 @@
-#!/usr/bin/env python3
-"""Minimum opreation """
+#!/usr/bin/python3
+''' minimun ops
+'''
 
 
 def minOperations(n):
-    """minimun ops
-    """
+    '''minimum operations
+    '''
     if not isinstance(n, int):
         return 0
-
-    ops_count = 0
-    clipboard = 0
+    count = 0
+    clp = 0
     done = 1
-
     while done < n:
-        if clipboard == 0:
-            # Initialize - copy all and first paste
-            clipboard = done
-            done += clipboard
-            ops_count += 2
+        if clp == 0:
+            clp = done
+            done += clp
+            count += 2
         elif n - done > 0 and (n - done) % done == 0:
-            # Copy all and paste to double characters
-            clipboard = done
-            done += clipboard
-            ops_count += 2
+            # copy all and paste
+            clp = done
+            done += clp
+            count += 2
+        elif clp > 0:
+            done += clp
+            count += 1
 
-        else:
-            # Paste to incrementally add characters
-            done += clipboard
-            ops_count += 1
-    return ops_count
+    return count
