5e49570c94c2ab36426d0f33f91ee5638ff4c81d
ayubsh <ibrahimayubsheikh@gmail.com>
2023-10-19 09:43:57 -0700

    parsing logs and stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,81 @@
-#!/usr/bin/env python3
-"""Log parsing"""
-import sys
-import signal
+#!/usr/bin/python3
+"""Log Parsing"""
+import re
 
 
-def print_stats(file_size, status_codes):
-    """prints the stats to the stdout
+def einput(input_line):
+    '''Extracts sections of a line of an HTTP request log.
+    '''
+    fp = (
+        r'\s*(?P<ip>\S+)\s*',
+        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
+        r'\s*"(?P<request>[^"]*)"\s*',
+        r'\s*(?P<status_code>\S+)',
+        r'\s*(?P<file_size>\d+)'
+    )
+    info = {
+        'status_code': 0,
+        'file_size': 0,
+    }
+    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
+    resp_match = re.fullmatch(log_fmt, input_line)
+    if resp_match is not None:
+        status_code = resp_match.group('status_code')
+        file_size = int(resp_match.group('file_size'))
+        info['status_code'] = status_code
+        info['file_size'] = file_size
+    return info
+
+
+def print_stats(total_file_size, status_codes_stats):
+    """prints staticsts to stdout
     """
-    print("File size: {}".format(file_size))
-    for code in sorted(status_codes.keys()):
-        print("{}: {}".format(code, status_codes[code]))
+    print('File size: {:d}'.format(total_file_size), flush=True)
+    for status_code in sorted(status_codes_stats.keys()):
+        num = status_codes_stats.get(status_code, 0)
+        if num > 0:
+            print('{:s}: {:d}'.format(status_code, num), flush=True)
 
 
-def signal_handler(sig, frame):
-    """handles singnals
+def umets(line, total_file_size, status_codes_stats):
+    """updates mats
     """
-    print_stats(file_size, status_codes)
-    sys.exit(0)
+    line_info = einput(line)
+    status_code = line_info.get('status_code', '0')
+    if status_code in status_codes_stats.keys():
+        status_codes_stats[status_code] += 1
+    return total_file_size + line_info['file_size']
 
 
-file_size = 0
-status_codes = {str(i): 0 for i in [200, 301, 400, 401, 403, 404, 405, 500]}
-line_count = 0
-
-signal.signal(signal.SIGINT, signal_handler)
-
-for line in sys.stdin:
+def run():
+    """runs log parsing
+    """
+    line_num = 0
+    total_file_size = 0
+    status_codes_stats = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0,
+    }
     try:
-        parts = line.split(" ")
-        if len(parts) < 9:
-            continue
-        size = int(parts[-1])
-        code = parts[-2]
-        if code in status_codes:
-            status_codes[code] += 1
-            file_size += size
-            line_count += 1
-            if line_count % 10 == 0:
-                print_stats(file_size, status_codes)
-    except ValueError:
-        continue
+        while True:
+            line = input()
+            total_file_size = umets(
+                line,
+                total_file_size,
+                status_codes_stats,
+            )
+            line_num += 1
+            if line_num % 10 == 0:
+                print_stats(total_file_size, status_codes_stats)
+    except (KeyboardInterrupt, EOFError):
+        print_stats(total_file_size, status_codes_stats)
 
-print_stats(file_size, status_codes)
+
+if __name__ == '__main__':
+    run()
